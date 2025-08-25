9f5776768631c2bacd8b260579f2b84ba6b2e0b7
ManojShrestha <shresthamanoj522@gmail.com>
2025-08-25 19:07:25 +0900

    feat: Add Export button
    Implemented Excel file generation from the contact table using Apache POI. Added UI button to trigger export.

--- src/test/java/com/scm/ScmApplicationTests.java
+++ src/test/java/com/scm/ScmApplicationTests.java
@@ -2,6 +2,7 @@ package com.scm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.scm.services.EmailService;
